03b378f9dbf8a29daf94ed5b8adb6e91bacac29f
Jesse Suderman <jessesuderman@gmail.com>
2020-06-08 20:56:20 -0700

    animations on scroll (animate.css)

--- resources/js/script.js
+++ resources/js/script.js
@@ -32,4 +32,29 @@ $(document).ready(function() {
       }
     });
   });
+
+  /* ANIMATIONS ON SCROLL */
+  $('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+  }, {
+    offset: '60%'
+  });
+
+  $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animate__animated animate__flipInX');
+  }, {
+    offset: '60%'
+  });
+
+  $('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').addClass('animate__animated animate__pulse');
+  }, {
+    offset: '60%'
+  });
 });
